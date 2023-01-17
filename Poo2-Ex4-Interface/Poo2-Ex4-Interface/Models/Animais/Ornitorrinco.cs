7c623a4fe8d278c1ef1657835f71adfdaa2829a4
Guilherme Laranjeira <guilr14@gmail.com>
2023-01-17 17:15:31 +0044

    Ex4 correcao ornitorrinco

--- Poo2-Ex4-Interface/Poo2-Ex4-Interface/Models/Animais/Ornitorrinco.cs
+++ Poo2-Ex4-Interface/Poo2-Ex4-Interface/Models/Animais/Ornitorrinco.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Poo2_Ex4_Interface.Models.Animais
 {
-    internal class Ornitorrinco
+    public class Ornitorrinco : Mamifero, IAquatico, IOviparo
     {
+        public bool ViveEmTerra { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool Mergulho { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool AguaDoce { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Botar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Chocar()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
